fbfb40774c9c95aeb8353f5f15023391d4365e4a
jshubham895 <jshubham895@gmail.com>
2021-08-25 22:57:38 +0530

    java

--- BinarySearch/src/com/leetcode/SortedBinaryTree.java
+++ BinarySearch/src/com/leetcode/SortedBinaryTree.java
@@ -2,6 +2,26 @@ package com.leetcode;
 
 public class SortedBinaryTree {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int[] a = {4,5,6,7,0,1,2};
+        System.out.println(findPivot(a));
+    }
+
+    static int findPivot(int[] a) {
+        int start = 0, end = a.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid < end && a[mid] > a[mid + 1])
+                return mid;
+            else if (mid > start && a[mid] < a[mid-1])
+                return mid - 1;
+            else if (a[mid] <= a[start])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return -1;
     }
 }
