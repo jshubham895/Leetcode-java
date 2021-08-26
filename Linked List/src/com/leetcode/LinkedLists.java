f34cb656987d465db6d5c46cb048a479676d1d0b
jshubham895 <jshubham895@gmail.com>
2021-08-26 21:56:44 +0530

    java

---
+++ Linked List/src/com/leetcode/LinkedLists.java
@@ -0,0 +1,15 @@
+package com.leetcode;
+
+import java.util.LinkedList;
+
+public class LinkedLists {
+    public static void main(String[] args) {
+        LinkedList<Integer> l = new LinkedList<>();
+
+        l.add(1);
+        l.add(2);
+        l.add(3);
+
+        System.out.println(l);
+    }
+}
